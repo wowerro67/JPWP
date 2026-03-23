d754deac686f1822ef707f61f7818030cec8903f
ArekK1223 <arokepa@student.agh.edu.pl>
2026-03-23 09:33:09 +0100

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -1,19 +1,16 @@
-from functools import reduce # Ta biblioteka przyda Ci się do rozwiązania!
+from functools import reduce 
 
 prices = [10, 20, 30.5, 15]
 total = 0
 
-# Podejście imperatywne: pętla modyfikująca stan zmiennej total
 for price in prices:
     total += price
     
-print("Suma imperatywna:", total) # 75.5
+print("Suma imperatywna:", total) 
 
-# ==========================================
-# TODO: Zrefaktoryzuj powyższy kod na styl funkcyjny!
+
+# Zrefaktoryzuj powyższy kod na styl funkcyjny!
 # 1. Pozbądź się pętli 'for' oraz zmiennej akumulującej 'total'.
 # 2. Użyj funkcji reduce() zaimportowanej na górze pliku.
 # 3. Zastosuj wyrażenie 'lambda' do zsumowania elementów listy.
-# ==========================================
 
-# Tutaj wpisz swój kod funkcyjny:
