19888f9892d53d84ff90ddc0b104aef2746b9ec4
ArekK1223 <arokepa@student.agh.edu.pl>
2026-03-23 09:32:04 +0100

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -9,13 +9,11 @@ for user in users:
     if user["is_active"]:
         active_user_names.append(user["name"])
         
-print("Wynik imperatywny:", active_user_names) # ['Anna', 'Maria']
+print("Wynik imperatywny:", active_user_names) 
 
-# ==========================================
-# TODO: Zrefaktoryzuj powyższy kod na styl funkcyjny!
+
+# Zrefaktoryzuj powyższy kod na styl funkcyjny
 # 1. Pozbądź się pętli 'for' oraz instrukcji 'if'.
 # 2. Użyj wbudowanych funkcji: map() oraz filter().
 # 3. Zastosuj wyrażenia 'lambda'.
-# ==========================================
 
-# Tutaj wpisz swój kod funkcyjny:
