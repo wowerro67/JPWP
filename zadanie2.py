f0f246b7951ff9a6380afdcfcc6da75dc8a1fe3d
ArekK1223 <arokepa@student.agh.edu.pl>
2026-03-23 09:32:43 +0100

    Update zadanie2.py

--- zadanie2.py
+++ zadanie2.py
@@ -1,21 +1,18 @@
 cart = ['Jabłka', 'Banany']
 
 def add_to_cart(item):
-    # Podejście imperatywne: modyfikujemy stan oryginalnej listy!
+
     cart.append(item) 
     return cart
 
 updated_cart = add_to_cart('Pomarańcze')
 
-print("Oryginalny koszyk po dodaniu:", cart) # Uwaga: oryginał został zmieniony!
+print("Oryginalny koszyk po dodaniu:", cart) 
 print("Zaktualizowany koszyk:", updated_cart)
 
-# ==========================================
-# TODO: Zrefaktoryzuj powyższy kod na styl funkcyjny!
+# Zrefaktoryzuj powyższy kod na styl funkcyjny
 # 1. Przerób 'add_to_cart' na czystą funkcję (pure function).
 # 2. Funkcja powinna przyjmować obecny koszyk oraz nowy element jako argumenty.
-# 3. Zastosuj zasadę niemutowalności (nie używaj metody .append()!).
+# 3. Zastosuj zasadę niemutowalności (nie używaj metody .append()).
 # 4. Funkcja ma zwracać nową listę, zostawiając oryginalny 'cart' bez zmian.
-# ==========================================
 
-# Tutaj wpisz swój kod funkcyjny:
